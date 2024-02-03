15971ab8b6e8e67a73c7e74d885e0d3b296a02af
Mateo Astelarra <mateo@astelarra.com>
2024-02-03 16:14:15 -0300

    Started with player character, fps game mode and builded vs project

---
+++ Source/FPSTime/FpsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FpsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTIME_API AFpsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
