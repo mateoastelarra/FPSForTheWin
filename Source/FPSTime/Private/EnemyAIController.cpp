344e2cb56569b57a9289f90d2697657ef509e12b
Mateo Astelarra <mateo@astelarra.com>
2024-02-12 18:44:45 -0300

    Enemy AI behavior

---
+++ Source/FPSTime/Private/EnemyAIController.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+void AEnemyAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	if (AIBehavior != nullptr)
+	{
+		RunBehaviorTree(AIBehavior);
+
+		APawn* Player = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+		UBlackboardComponent* AIBlackboard = GetBlackboardComponent();
+		AIBlackboard->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
+	}
+}
+
+void AEnemyAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
