88515562e44c0396351219e002139061517a7836
Mateo Astelarra <mateo@astelarra.com>
2024-03-18 16:09:48 -0300

    Mutant enemy character

---
+++ Source/FPSTime/Public/MutantEnemyCharacter.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEnemyCharacter.h"
+#include "MutantEnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTIME_API AMutantEnemyCharacter : public ABaseEnemyCharacter
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Attack() override;
+};
